1843f5d291aca2631149b7b9d08b8d5a4abca3d1
KlausSeer <u201710889@upc.edu.pe>
2020-05-30 11:01:51 -0500

    Pregunta 4

---
+++ 4/Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Node[] nodes;
+    public GameObject Panel;
+
+    bool Victory()
+    {
+        foreach (Node node in nodes)
+        {
+            if (!node.Connected)
+                return false;
+        }
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Victory())
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
---
+++ 4/Assets/Scripts/Node.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int index;
+    public int index2 = -1;
+    public Node[] Adj;
+    public Node Pair;
+    public bool Connected;
+    RectTransform trans;
+
+    int Opposite(int index)
+    {
+        return (index + 2) % 4;
+    }
+
+    void Check()
+    {
+            Node n = Adj[index];
+            if (n != null)
+            {
+                n.Connect(this);
+            }
+        //else
+        //{
+        //    Node n = Adj[index];
+        //    if (n != null)
+        //    {
+        //        if(n.index2 != -1)
+        //        {
+        //            Connect2(Pair, true);
+        //        }
+        //    }
+        //}
+    }
+
+    void Connect2(Node me, bool first)
+    {
+        if(first)
+        {
+            Node n = Adj[index2];
+            if (n!= null)
+            {
+                if(n.index2 == -1)
+                {
+                    if (n.index == Opposite(index2) && !n.Connected)
+                    {
+                        n.Connected = true;
+                        Connected = true;
+                        n.Pair = me;
+                    }
+                }
+                else
+                {
+                    if (n.index == Opposite(index2))
+                    {
+                        n.Connected = true;
+                        n.Pair = me;
+                        n.Connect2(me, true);
+                    }
+                    else if (n.index2 == Opposite(index2))
+                    {
+                        n.Connected = true;
+                        n.Pair = me;
+                        n.Connect2(me, false);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Node n = Adj[index];
+            if (n != null)
+            {
+                if (n.index2 == -1)
+                {
+                    if (n.index == Opposite(index) && !n.Connected)
+                    {
+                        n.Connected = true;
+                        Connected = true;
+                        n.Pair = me;
+                    }
+                }
+                else
+                {
+                    if (n.index == Opposite(index))
+                    {
+                        n.Connected = true;
+                        n.Pair = me;
+                        n.Connect2(me, true);
+                    }
+                    else if (n.index2 == Opposite(index))
+                    {
+                        n.Connected = true;
+                        n.Pair = me;
+                        n.Connect2(me, false);
+                    }
+                }
+            }
+        }
+    }
+
+    void Connect(Node me)
+    {
+        if(index2 == -1)
+        {
+            if(index == Opposite(me.index))
+            {
+                Connected = true;
+                me.Connected = true;
+                Pair = me;
+                me.Pair = this;
+            }
+           
+        }
+        else
+        {
+            if(index == Opposite(me.index))
+            {
+                Connected = true;
+                me.Connected = true;
+                Pair = me;
+                me.Pair = this;
+                Connect2(me, true);
+            }
+            else if(index2 == Opposite(me.index))
+            {
+                Connected = true;
+                me.Connected = true;
+                Pair = me;
+                me.Pair = this;
+                Connect2(me, false);
+            }
+        }
+    }
+
+    void Rotate()
+    {
+        if(index2 == -1)
+        {
+            float rotation = 90 * (index -1);
+            trans.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, -rotation));
+        }
+        else if(index == Opposite(index2))
+        {
+            float rotation = 90 * (index - 1);
+            trans.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, -rotation));
+        }
+        else
+        {
+            float rotation = 90 * (index - 3);
+            trans.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, -rotation));
+        }
+    }
+
+    public void Click()
+    {
+        if (index2 != -1)
+        {
+            index2++;
+            index2 %= 4;
+        }
+        if(Pair != null)
+        {
+            if(index2 == -1)
+            {
+                if(Pair.index == Opposite(index))
+                {
+
+                    Pair.Connected = false;
+                    Pair = null;
+                }
+            }
+        }
+        Connected = false;   
+        index++;
+        index %= 4;
+        Rotate();
+        Check();
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<RectTransform>();
+        Rotate();
+        Check();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
